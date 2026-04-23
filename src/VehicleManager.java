141859978d01f05fbe50ff01038d62f6ba2855c6
Ibrahim <yassinibrahimyassin7@gmail.com>
2026-04-23 11:02:03 +0200

    aa

--- src/VehicleManager.java
+++ src/VehicleManager.java
@@ -1,16 +1,19 @@
 import java.util.List;
+
+
 public class VehicleManager{
     private final VehicleRepository vehicleRepository;
-    private final DriverRepository driverRepository;
 
-    public VehicleManager(VehicleRepository vr, DriverRepository dr) {
+    public VehicleManager(VehicleRepository vr) {
         this.vehicleRepository = vr;
-        this.driverRepository = dr;
     }
     public Vehicle registerVehicle(String driverId,String model, int year,String color ,String plateNumber,VehicleType type){
         if (vehicleRepository.existsByPlateNumber(plateNumber)) {
             throw new IllegalArgumentException("Plate number "+plateNumber+" isalready used ");
         }
+        List<Vehicle> driverVehicles=vehicleRepository.findByDriverID(driverId);
+        if(!driverVehicles.isEmpty() )
+            throw new IllegalStateException("Driver ID: "+ driverId+" is already registered. ");
         Vehicle newVehicle=new Vehicle(driverId,model,year,color,plateNumber,type);
         vehicleRepository.save(newVehicle);
         return newVehicle;
